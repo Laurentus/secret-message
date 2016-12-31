8297dc5d1dc07832772f272ebbfa2b90c4f37c14
Lauri Oikari <lauri.oikari1+github@gmail.com>
2016-12-31 17:10:30 +0200

    Draw dem lines

--- turtle/turtle.go
+++ turtle/turtle.go
@@ -8,12 +8,14 @@ import (
 
 var drawColor = color.RGBA{0, 0, 0, 255}
 var secret *image.RGBA
+var code image.Image
 
 const (
 	Up = iota
 	Right
 	Down
 	Left
+	NoDir
 )
 
 const (
@@ -40,20 +42,16 @@ var colorMap = map[color.Color]int{
 }
 
 func Decrypt(m image.Image) image.Image {
+	code = m
 	bounds := m.Bounds()
 	secret = image.NewRGBA(bounds)
-	fmt.Println(m.ColorModel())
-	drawing := false
 
 	fmt.Println("Image size is ", bounds.Min, bounds.Max)
 	for y := bounds.Max.Y - 1; y >= 0; y-- {
 		for x := bounds.Max.X - 1; x >= 0; x-- {
 			command := colorMap[m.At(x, y)]
-			if drawing {
-				secret.Set(x, y, drawColor)
-				drawing = command == NoCommand || command == GoLeft
-			} else if command == GoLeft {
-				drawing = true
+			if command == GoUp || command == GoLeft {
+				DrawNextLine(x, y, command, NoDir)
 			}
 		}
 	}
@@ -61,10 +59,60 @@ func Decrypt(m image.Image) image.Image {
 	return secret
 }
 
+func DrawLine(x, y, direction int) {
+	curX := x
+	curY := y
+	command := NoCommand
+	for command == NoCommand {
+		switch direction {
+		case Up:
+			curY--
+			break
+		case Down:
+			curY++
+			break
+		case Left:
+			curX--
+			break
+		case Right:
+			curX++
+			break
+		}
+		command = colorMap[code.At(curX, curY)]
+	}
+
+	if direction == Left || direction == Up {
+		DrawRect(image.Pt(curX, curY), image.Pt(x, y))
+	} else {
+		DrawRect(image.Pt(x, y), image.Pt(curX, curY))
+	}
+
+	if command != Stop {
+		DrawNextLine(curX, curY, command, direction)
+	}
+}
+
+func DrawNextLine(x, y, command, direction int) {
+	switch command {
+	case GoUp:
+		DrawLine(x, y, Up)
+		break
+	case GoLeft:
+		DrawLine(x, y, Left)
+		break
+	case TurnR:
+		DrawLine(x, y, (direction+1)%4)
+		break
+	case TurnL:
+		DrawLine(x, y, (direction+3)%4)
+		break
+	}
+}
+
 // Draw a line between points p1 and p2
 func DrawRect(p1, p2 image.Point) {
-	for x := p1.X; x < p2.X; x++ {
-		for y := p1.Y; y < p2.Y; y++ {
+	for x := p1.X; x <= p2.X; x++ {
+		for y := p1.Y; y <= p2.Y; y++ {
 			secret.Set(x, y, drawColor)
 		}
 	}
